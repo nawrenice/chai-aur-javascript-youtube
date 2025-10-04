da7a16654242941ccecede4c77237cf6c37730bc
Naren Yadav <nawrenice10@gmail.com>
2025-10-04 05:34:31 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as a newer version
+
+// alert(3+3)  //we are using nodejs, not browser
+
+//code readibility example
+console.log(3               //not a good practice
+    +3
+);              
+
+console.log(3+3)    //is a good practice
+
+
+//DATATYPES
+// number ==> 2^53
+// bigint 
+// string ==> ""
+// boolean ==> true/false
+// null ==> standalone value 
+// undefined ==>        eg. let state;
+// symbol ==> unique
+
+
+// object
+
+console.log(typeof "Naren")
+console.log(typeof age)
+console.log(typeof null)        //null is object data type
